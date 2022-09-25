93a6c042d3193eed4b6d32093e8e84373e7dfcd0
Kelvin-Yeoh <62163423+Kelvin-Yeoh@users.noreply.github.com>
2022-09-25 21:35:39 +0800

    eventview
    user can click the event

--- app/src/main/java/com/example/testing/EventView.kt
+++ app/src/main/java/com/example/testing/EventView.kt
@@ -1,7 +1,13 @@
 package com.example.testing
 
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.os.AsyncTask
 import android.os.Bundle
 import android.util.Log
+import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.net.toUri
@@ -12,19 +18,25 @@ import com.android.volley.Response
 import com.android.volley.toolbox.JsonArrayRequest
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
+import kotlinx.android.synthetic.main.rv_one.*
+import kotlinx.android.synthetic.main.rv_one.view.*
 import org.json.JSONArray
+import org.json.JSONException
 import org.json.JSONObject
 
 
-class EventView : AppCompatActivity() {
+class EventView : AppCompatActivity() ,onClickListener{
 
     private val URLstring = "http://10.0.2.2/getevent.php"
     private lateinit var rvAdapter : RvAdapter
     private var recyclerView: RecyclerView? = null
+    val data = ArrayList<ItemsViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.donate_screen)
+        title = "KotlinApp"
+
         // getting the recyclerview by its id
         recyclerView = findViewById<RecyclerView>(R.id.recycler)
         // this creates a vertical layout Manager
@@ -37,7 +49,7 @@ class EventView : AppCompatActivity() {
         val stringRequest: JsonArrayRequest = object : JsonArrayRequest(
             Request.Method.GET, URLstring,
             null, { response ->
-                val data = ArrayList<ItemsViewModel>()
+
                 for (i in 0 until response.length()) {
                     val dataobj = response.getJSONObject(i)
 
@@ -50,7 +62,7 @@ class EventView : AppCompatActivity() {
                         )
                     )
                 }
-                val adapter = RvAdapter(data)
+                val adapter = RvAdapter(data, this)
                 recyclerview.adapter = adapter
             },
             Response.ErrorListener { error ->
@@ -63,6 +75,13 @@ class EventView : AppCompatActivity() {
         }
         val requestQueue = Volley.newRequestQueue(applicationContext)
         requestQueue.add(stringRequest)
+    }
+
+    override fun onItemClick(position: Int) {
+        Toast.makeText(this, "Event"+position+"Clicked", Toast.LENGTH_SHORT).show()
+        val intent = Intent(this, EventDetail::class.java)
+        intent.putExtra("textTitle", data[position].textViewEvent)
+        startActivity(intent)
     }
 }
 
